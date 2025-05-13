36a56054c78baf4d654e6b5d85e1939a91b2ef70
Maggy <yonezawaaini@gmail.com>
2025-05-13 14:22:33 +0800

    簡易搜尋API

---
+++ TravelAgencyFrontendAPI/Controllers/OfficialTravelControllers/OfficialSearchController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelAgencyFrontendAPI.Data;
+using TravelAgencyFrontendAPI.DTOs.OfficialDTOs.Search;
+using TravelAgencyFrontendAPI.Models;
+
+namespace TravelAgencyFrontendAPI.Controllers.OfficialTravelControllers
+{
+    public class OfficialSearchController : Controller
+    {
+        private readonly AppDbContext _context;
+        public OfficialSearchController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("search")]
+        public async Task<IEnumerable<SearchBoxResultDTO>> Search([FromBody] SearchBoxInputDTO searchBoxInput)
+        {
+                
+
+            return _context.OfficialTravels
+                .Where(t => t.Title.Contains(searchBoxInput.Destination) || t.Description.Contains(searchBoxInput.Destination) && t.Status == TravelStatus.Active)
+                .Select(t => new SearchBoxResultDTO
+                {
+                    Id = t.OfficialTravelId,
+                    Title = t.Title,
+                    Description = t.Description
+                    
+                });
+        }
+    }
+}
---
+++ TravelAgencyFrontendAPI/DTOs/OfficialDTOs/Search/SearchBoxInputDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAgencyFrontendAPI.DTOs.OfficialDTOs.Search
+{
+    public class SearchBoxInputDTO
+    {
+        public string? Destination { get; set; }   // 可輸入「日本」、「北海道」等
+        //public int? PeopleCount { get; set; }      // 人數（選填）
+        //public DateTime? StartDate { get; set; }   // 出發日（選填）
+        //public DateTime? EndDate { get; set; }     // 結束日（選填）
+
+    }
+}
