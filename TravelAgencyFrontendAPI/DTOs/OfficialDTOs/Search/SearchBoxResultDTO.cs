258a10bcf07ba703a907dc369588c8aad3c81891
Maggy <yonezawaaini@gmail.com>
2025-05-14 22:02:27 +0800

    完成簡易搜尋引導至詳情頁

--- TravelAgencyFrontendAPI/DTOs/OfficialDTOs/Search/SearchBoxResultDTO.cs
+++ TravelAgencyFrontendAPI/DTOs/OfficialDTOs/Search/SearchBoxResultDTO.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } // 旅遊行程的唯一識別碼
         public string Title { get; set; } // 旅遊行程的名稱
         public string Description { get; set; } // 旅遊行程的描述
-        //public decimal Price { get; set; } // 旅遊行程的價格
+        //public decimal? Price { get; set; } // 旅遊行程的價格
         //public string? Cover { get; set; }
     }
 }
