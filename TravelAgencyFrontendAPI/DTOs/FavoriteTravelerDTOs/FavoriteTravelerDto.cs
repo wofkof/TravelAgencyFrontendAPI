0a2a0b2bfe07012ea5d487b9a8ef1d24c66bcd57
ST <shanna869724@gmail.com>
2025-05-15 18:11:34 +0800

    常用旅客清單bug修復

--- TravelAgencyFrontendAPI/DTOs/FavoriteTravelerDTOs/FavoriteTravelerDto.cs
+++ TravelAgencyFrontendAPI/DTOs/FavoriteTravelerDTOs/FavoriteTravelerDto.cs
@@ -8,11 +8,11 @@ namespace TravelAgencyFrontendAPI.DTOs.FavoriteTravelerDTOs
         public int MemberId { get; set; }
 
         public string Name { get; set; } = null!;
-        public string Phone { get; set; } = null!;
+        public string? Phone { get; set; }
         public string IdNumber { get; set; } = null!;
         public DateTime BirthDate { get; set; }
         public GenderType? Gender { get; set; }
-        public string Email { get; set; } = null;
+        public string? Email { get; set; }
         public DocumentType? DocumentType { get; set; }
         public string? DocumentNumber { get; set; }
         public string? PassportSurname { get; set; }
