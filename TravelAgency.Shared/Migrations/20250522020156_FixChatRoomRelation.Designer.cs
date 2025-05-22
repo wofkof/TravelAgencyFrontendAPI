7d7fb5d85e7b51f5eaea8f002672bb4d58a190d1
wofkof <qwex0088@gmail.com>
2025-05-22 11:00:47 +0800

    優化聊天室卡片顯示

--- TravelAgency.Shared/Migrations/20250522020156_FixChatRoomRelation.Designer.cs
+++ TravelAgency.Shared/Migrations/20250522020156_FixChatRoomRelation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelAgency.Shared.Data;
 
@@ -11,9 +12,11 @@ using TravelAgency.Shared.Data;
 namespace TravelAgency.Shared.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250522020156_FixChatRoomRelation")]
+    partial class FixChatRoomRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TravelAgency.Shared/Migrations/20250522020156_FixChatRoomRelation.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAgency.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChatRoomRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_T_ChatRoom_EmployeeId",
+                table: "T_ChatRoom");
+
+            migrationBuilder.DropIndex(
+                name: "IX_T_ChatRoom_MemberId",
+                table: "T_ChatRoom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_ChatRoom_EmployeeId_MemberId",
+                table: "T_ChatRoom",
+                columns: new[] { "EmployeeId", "MemberId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_ChatRoom_MemberId",
+                table: "T_ChatRoom",
+                column: "MemberId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_T_ChatRoom_EmployeeId_MemberId",
+                table: "T_ChatRoom");
+
+            migrationBuilder.DropIndex(
+                name: "IX_T_ChatRoom_MemberId",
+                table: "T_ChatRoom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_ChatRoom_EmployeeId",
+                table: "T_ChatRoom",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_ChatRoom_MemberId",
+                table: "T_ChatRoom",
+                column: "MemberId",
+                unique: true);
+        }
+    }
+}
