8516363bd65eb104a550ef0f01350dde8e941304
Maggy <yonezawaaini@gmail.com>
2025-05-22 21:34:02 +0800

    熱門行程完成

---
+++ TravelAgencyFrontendAPI/DTOs/OfficialDTOs/IndexCard.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgencyFrontendAPI.DTOs.OfficialDTOs
+{
+    public class IndexCard:baseDTO
+    {
+        public decimal? Price { get; set; }
+        public int DetailId { get; set; }
+        public int GroupId { get; set; }
+
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int? Days { get; set; }
+    }
+}
